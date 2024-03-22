76377a253b6b4733c9a5e7243966ba174d5ce041
Marek <marekkrupa03@gmail.com>
2024-03-22 20:49:57 +0100

    countries list layout

--- src/app/shared/components/countries-list/countries-list.component.ts
+++ src/app/shared/components/countries-list/countries-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Country } from '../../models/country.model';
+import { Countries } from '../../mock-countries';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-countries-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CountryDetailComponent],
   templateUrl: './countries-list.component.html',
-  styleUrl: './countries-list.component.scss'
+  styleUrl: './countries-list.component.scss',
 })
 export class CountriesListComponent {
-
+  countries: Country[] = Countries;
 }
