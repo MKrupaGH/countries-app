75b5451e22716336751e1ff3cf1da57388ca399f
Marek <marekkrupa03@gmail.com>
2024-03-22 20:49:08 +0100

    page ui updated

--- src/app/countries/pages/countries-page/countries-page.component.ts
+++ src/app/countries/pages/countries-page/countries-page.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
 import { CountriesListComponent } from '../../../shared/components/countries-list/countries-list.component';
-import { FilterComponent } from '../../../shared/components/filter/filter.component';
-import { FooterComponent } from '../../../shared/components/footer/footer.component';
-import { IndependentCheckComponent } from '../../../shared/components/independent-check/independent-check.component';
-import { SearchComponent } from '../../../shared/components/search/search.component';
+
+import { SearchWindowComponent } from '../../components/search-window/search-window.component';
 import { PaginatorComponent } from '../../../shared/components/paginator/paginator.component';
 
 @Component({
   selector: 'app-countries-page',
   standalone: true,
-  imports: [
-    CountriesListComponent,
-    FilterComponent,
-    IndependentCheckComponent,
-    CountriesPageComponent,
-    SearchComponent,
-    PaginatorComponent,
-  ],
+  imports: [SearchWindowComponent, CountriesListComponent, PaginatorComponent],
   templateUrl: './countries-page.component.html',
   styleUrl: './countries-page.component.scss',
 })
