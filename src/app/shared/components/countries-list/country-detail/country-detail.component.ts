f0fb961d35dc7b9879d8b61bc5e8e9bb55e0e830
Marek <marekkrupa03@gmail.com>
2024-03-22 20:50:42 +0100

    layout ready

--- src/app/shared/components/countries-list/country-detail/country-detail.component.ts
+++ src/app/shared/components/countries-list/country-detail/country-detail.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Country } from '../../../models/country.model';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-country-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './country-detail.component.html',
-  styleUrl: './country-detail.component.scss'
+  styleUrl: './country-detail.component.scss',
 })
 export class CountryDetailComponent {
-
+  @Input() country!: Country;
 }
