e2563229b4f095af080df7565fa7c0c381e80522
Marek <marekkrupa03@gmail.com>
2024-03-22 20:49:34 +0100

    mocked countries

---
+++ src/app/shared/mock-countries.ts
@@ -0,0 +1,296 @@
+import { Country } from './models/country.model';
+
+export const Countries: Country[] = [
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/cy.png',
+      svg: 'https://flagcdn.com/cy.svg',
+      alt: 'The flag of Cyprus has a white field, at the center of which is a copper-colored silhouette of the Island of Cyprus above two green olive branches crossed at the stem.',
+    },
+    name: {
+      common: 'Cyprus',
+      official: 'Republic of Cyprus',
+      nativeName: {
+        ell: {
+          official: 'Δημοκρατία της Κύπρος',
+          common: 'Κύπρος',
+        },
+        tur: {
+          official: 'Kıbrıs Cumhuriyeti',
+          common: 'Kıbrıs',
+        },
+      },
+    },
+    cca2: 'CY',
+    capital: ['Nicosia'],
+    region: 'Europe',
+  },
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/er.png',
+      svg: 'https://flagcdn.com/er.svg',
+      alt: 'The flag of Eritrea comprises three triangles — a large red isosceles triangle with its base spanning the hoist end and its apex at the midpoint on the fly end, and a green and blue right-angled triangle above and beneath the red triangle. On the hoist side of the red triangle is a golden vertical olive branch encircled by a golden olive wreath.',
+    },
+    name: {
+      common: 'Eritrea',
+      official: 'State of Eritrea',
+      nativeName: {
+        ara: {
+          official: 'دولة إرتريا',
+          common: 'إرتريا‎',
+        },
+        eng: {
+          official: 'State of Eritrea',
+          common: 'Eritrea',
+        },
+        tir: {
+          official: 'ሃገረ ኤርትራ',
+          common: 'ኤርትራ',
+        },
+      },
+    },
+    cca2: 'ER',
+    capital: ['Asmara'],
+    region: 'Africa',
+  },
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/lr.png',
+      svg: 'https://flagcdn.com/lr.svg',
+      alt: 'The flag of Liberia is composed of eleven equal horizontal bands of red alternating with white. A blue square bearing a five-pointed white star is superimposed in the canton.',
+    },
+    name: {
+      common: 'Liberia',
+      official: 'Republic of Liberia',
+      nativeName: {
+        eng: {
+          official: 'Republic of Liberia',
+          common: 'Liberia',
+        },
+      },
+    },
+    cca2: 'LR',
+    capital: ['Monrovia'],
+    region: 'Africa',
+  },
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/va.png',
+      svg: 'https://flagcdn.com/va.svg',
+      alt: 'The flag of Vatican City is square shaped. It is composed of two equal vertical bands of yellow and white, with national coat of arms centered in the white band. The national coat of arms comprises the Papal Tiara superimposed on two crossed keys.',
+    },
+    name: {
+      common: 'Vatican City',
+      official: 'Vatican City State',
+      nativeName: {
+        ita: {
+          official: 'Stato della Città del Vaticano',
+          common: 'Vaticano',
+        },
+        lat: {
+          official: 'Status Civitatis Vaticanæ',
+          common: 'Vaticanæ',
+        },
+      },
+    },
+    cca2: 'VA',
+    capital: ['Vatican City'],
+    region: 'Europe',
+  },
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/so.png',
+      svg: 'https://flagcdn.com/so.svg',
+      alt: 'The flag of Somalia features a large five-pointed white star centered on a light blue field.',
+    },
+    name: {
+      common: 'Somalia',
+      official: 'Federal Republic of Somalia',
+      nativeName: {
+        ara: {
+          official: 'جمهورية الصومال‎‎',
+          common: 'الصومال‎‎',
+        },
+        som: {
+          official: 'Jamhuuriyadda Federaalka Soomaaliya',
+          common: 'Soomaaliya',
+        },
+      },
+    },
+    cca2: 'SO',
+    capital: ['Mogadishu'],
+    region: 'Africa',
+  },
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/zm.png',
+      svg: 'https://flagcdn.com/zm.svg',
+      alt: 'The flag of Zambia has a green field, on the fly side of which is a soaring orange African fish eagle above a rectangular area divided into three equal vertical bands of red, black and orange.',
+    },
+    name: {
+      common: 'Zambia',
+      official: 'Republic of Zambia',
+      nativeName: {
+        eng: {
+          official: 'Republic of Zambia',
+          common: 'Zambia',
+        },
+      },
+    },
+    cca2: 'ZM',
+    capital: ['Lusaka'],
+    region: 'Africa',
+  },
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/ve.png',
+      svg: 'https://flagcdn.com/ve.svg',
+      alt: 'The flag of Venezuela is composed of three equal horizontal bands of yellow, blue and red. At the center of the blue band are eight five-pointed white stars arranged in a horizontal arc.',
+    },
+    name: {
+      common: 'Venezuela',
+      official: 'Bolivarian Republic of Venezuela',
+      nativeName: {
+        spa: {
+          official: 'República Bolivariana de Venezuela',
+          common: 'Venezuela',
+        },
+      },
+    },
+    cca2: 'VE',
+    capital: ['Caracas'],
+    region: 'Americas',
+  },
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/tm.png',
+      svg: 'https://flagcdn.com/tm.svg',
+      alt: 'The flag of Turkmenistan has a green field. It features a red vertical band, bearing five carpet guls stacked above two crossed olive branches, near the hoist end of the field. Just to the fly side of the vertical band near the top edge of the field is a hoist-side facing white crescent and five small five-pointed white stars placed just outside the crescent opening.',
+    },
+    name: {
+      common: 'Turkmenistan',
+      official: 'Turkmenistan',
+      nativeName: {
+        rus: {
+          official: 'Туркменистан',
+          common: 'Туркмения',
+        },
+        tuk: {
+          official: 'Türkmenistan',
+          common: 'Türkmenistan',
+        },
+      },
+    },
+    cca2: 'TM',
+    capital: ['Ashgabat'],
+    region: 'Asia',
+  },
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/al.png',
+      svg: 'https://flagcdn.com/al.svg',
+      alt: 'The flag of Albania features a silhouetted double-headed black eagle at the center of a red field.',
+    },
+    name: {
+      common: 'Albania',
+      official: 'Republic of Albania',
+      nativeName: {
+        sqi: {
+          official: 'Republika e Shqipërisë',
+          common: 'Shqipëria',
+        },
+      },
+    },
+    cca2: 'AL',
+    capital: ['Tirana'],
+    region: 'Europe',
+  },
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/hr.png',
+      svg: 'https://flagcdn.com/hr.svg',
+      alt: 'The flag of Croatia is composed of three equal horizontal bands of red, white and blue, with coat of arms of Croatia superimposed in the center.',
+    },
+    name: {
+      common: 'Croatia',
+      official: 'Republic of Croatia',
+      nativeName: {
+        hrv: {
+          official: 'Republika Hrvatska',
+          common: 'Hrvatska',
+        },
+      },
+    },
+    cca2: 'HR',
+    capital: ['Zagreb'],
+    region: 'Europe',
+  },
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/gb.png',
+      svg: 'https://flagcdn.com/gb.svg',
+      alt: 'The flag of the United Kingdom — the Union Jack — has a blue field. It features the white-edged red cross of Saint George superimposed on the diagonal red cross of Saint Patrick which is superimposed on the diagonal white cross of Saint Andrew.',
+    },
+    name: {
+      common: 'United Kingdom',
+      official: 'United Kingdom of Great Britain and Northern Ireland',
+      nativeName: {
+        eng: {
+          official: 'United Kingdom of Great Britain and Northern Ireland',
+          common: 'United Kingdom',
+        },
+      },
+    },
+    cca2: 'GB',
+    capital: ['London'],
+    region: 'Europe',
+  },
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/sd.png',
+      svg: 'https://flagcdn.com/sd.svg',
+      alt: 'The flag of Sudan is composed of three equal horizontal bands of red, white and black, with a green isosceles triangle superimposed on the hoist side. The green triangle spans about two-fifth the width of the field with its base on the hoist end.',
+    },
+    name: {
+      common: 'Sudan',
+      official: 'Republic of the Sudan',
+      nativeName: {
+        ara: {
+          official: 'جمهورية السودان',
+          common: 'السودان',
+        },
+        eng: {
+          official: 'Republic of the Sudan',
+          common: 'Sudan',
+        },
+      },
+    },
+    cca2: 'SD',
+    capital: ['Khartoum'],
+    region: 'Africa',
+  },
+  {
+    flags: {
+      png: 'https://flagcdn.com/w320/tl.png',
+      svg: 'https://flagcdn.com/tl.svg',
+      alt: 'The flag of Timor-Leste has a red field with two isosceles triangles which share a common base on the hoist end. The smaller black triangle, which bears a five-pointed white star at its center and spans one-third the width of the field, is superimposed on the larger yellow triangle that extends to the center of the field.',
+    },
+    name: {
+      common: 'Timor-Leste',
+      official: 'Democratic Republic of Timor-Leste',
+      nativeName: {
+        por: {
+          official: 'República Democrática de Timor-Leste',
+          common: 'Timor-Leste',
+        },
+        tet: {
+          official: 'Repúblika Demokrátika Timór-Leste',
+          common: 'Timór-Leste',
+        },
+      },
+    },
+    cca2: 'TL',
+    capital: ['Dili'],
+    region: 'Asia',
+  },
+];
