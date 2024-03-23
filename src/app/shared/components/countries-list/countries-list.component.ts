50e9abd00c8033858054d7a682fdc92b8fe9308d
Marek <marekkrupa03@gmail.com>
2024-03-23 17:20:43 +0100

    countries from store

--- src/app/shared/components/countries-list/countries-list.component.ts
+++ src/app/shared/components/countries-list/countries-list.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../models/country.model';
 import { Countries } from '../../mock-countries';
 import { CountryDetailComponent } from './country-detail/country-detail.component';
 import { CommonModule } from '@angular/common';
-
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { getAllCountries } from '../../../store/countries.actions';
+import { getCountries } from '../../../store/countries.selector';
+import { setLoadingSpinner } from '../../../store/shared/shared.actions';
 @Component({
   selector: 'app-countries-list',
   standalone: true,
@@ -11,6 +15,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './countries-list.component.html',
   styleUrl: './countries-list.component.scss',
 })
-export class CountriesListComponent {
+export class CountriesListComponent implements OnInit {
+  countries$!: Observable<Country[]>;
   countries: Country[] = Countries;
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(setLoadingSpinner({ status: true }));
+    this.store.dispatch(getAllCountries());
+    this.countries$ = this.store.select(getCountries);
+  }
 }
