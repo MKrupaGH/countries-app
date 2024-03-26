25d8a58439156caa1c799582314993b53081c5a2
Marek <marekkrupa03@gmail.com>
2024-03-26 17:51:45 +0100

    refactored to valueChanges service

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { SearchedService } from './searched.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -16,15 +22,24 @@ import { SearchedService } from './searched.service';
       FormsModule,
       MatButtonModule,
       MatIconModule,
+      ReactiveFormsModule,
     ],
   ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
   constructor(private searchedService: SearchedService) {}
 
-  onChange(search: string) {
-    this.searchedService.searchQuery = search;
+  searchForm = new FormGroup({
+    search: new FormControl(''),
+  });
+
+  ngOnInit(): void {
+    this.searchForm
+      .get('search')
+      ?.valueChanges.subscribe(
+        (search) => (this.searchedService.searchQuery = search!)
+      );
   }
 }
