2d7f39d7be754d9a325b11eda657ffe76b4c6b96
Marek <marekkrupa03@gmail.com>
2024-03-26 16:58:13 +0100

    search feature added

--- src/app/shared/components/countries-list/countries-list.component.ts
+++ src/app/shared/components/countries-list/countries-list.component.ts
@@ -3,51 +3,64 @@ import { Country } from '../../models/country.model';
 import { Countries } from '../../mock-countries';
 import { CountryDetailComponent } from './country-detail/country-detail.component';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import { Observable, combineLatestWith, map } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { getAllCountries } from '../../../store/countries.actions';
 import {
-  getCountries,
+  getAllToSearch,
   getCountriesLength,
 } from '../../../store/countries.selector';
 import { setLoadingSpinner } from '../../../store/shared/shared.actions';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { Constants } from '../../constant';
+import { SearchedService } from '../search/searched.service';
+import { PaginatorPipe } from '../../pipes/paginator.pipe';
+
 @Component({
   selector: 'app-countries-list',
   standalone: true,
-  imports: [CommonModule, CountryDetailComponent, MatPaginatorModule],
+  imports: [
+    CommonModule,
+    CountryDetailComponent,
+    MatPaginatorModule,
+    PaginatorPipe,
+  ],
   templateUrl: './countries-list.component.html',
   styleUrl: './countries-list.component.scss',
 })
 export class CountriesListComponent implements OnInit {
   countries$!: Observable<Country[]>;
   countriesLength$!: Observable<number>;
-  //countries: Country[] = Countries;
 
   pageSize = Constants.paginatorConstants.defaultPageSize;
-  pageIndex = 1;
+  pageIndex = 0;
   pageSizeOptions = Constants.paginatorConstants.pageSizeOptions;
   showPageSizeOptions = true;
   showFirstLastButtons = true;
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private searchedService: SearchedService) {}
 
   ngOnInit(): void {
     this.store.dispatch(setLoadingSpinner({ status: true }));
-    this.store.dispatch(getAllCountries());
+    this.store.dispatch(getAllCountries({ independent: true }));
     this.countriesLength$ = this.store.select(getCountriesLength);
-    this.countries$ = this.store.select(
-      getCountries(this.pageSize, this.pageIndex)
+    this.countries$ = this.store.select(getAllToSearch).pipe(
+      combineLatestWith(this.searchedService.searchQuery),
+      map(([countries, searchQuery]) =>
+        countries.filter((country) =>
+          country.name.common
+            .toLowerCase()
+            .startsWith(searchQuery.toLowerCase())
+        )
+      )
     );
   }
 
   handlePageEvent($event: PageEvent) {
     this.pageSize = $event.pageSize;
     this.pageIndex = $event.pageIndex;
-    this.countries$ = this.store.select(
-      getCountries(this.pageSize, this.pageIndex + 1)
-    );
     window.scroll(0, 0);
   }
+
+  handleSearch() {}
 }
