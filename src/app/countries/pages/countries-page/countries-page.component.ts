e448b47961de1a0bbe2524c471c5b43b4e12fa1c
Marek <marekkrupa03@gmail.com>
2024-03-30 21:33:53 +0100

    Paginator component deleted

--- src/app/countries/pages/countries-page/countries-page.component.ts
+++ src/app/countries/pages/countries-page/countries-page.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CountriesListComponent } from '../../../shared/components/countries-list/countries-list.component';
 
 import { SearchWindowComponent } from '../../components/search-window/search-window.component';
-import { PaginatorComponent } from '../../../shared/components/paginator/paginator.component';
 
 @Component({
   selector: 'app-countries-page',
   standalone: true,
-  imports: [SearchWindowComponent, CountriesListComponent, PaginatorComponent],
+  imports: [SearchWindowComponent, CountriesListComponent],
   templateUrl: './countries-page.component.html',
   styleUrl: './countries-page.component.scss',
 })
