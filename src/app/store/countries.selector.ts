64d319d43c2ac71cb223d0e7b02ee1959fccd750
Marek <marekkrupa03@gmail.com>
2024-03-30 21:34:14 +0100

    additional selector deleted

--- src/app/store/countries.selector.ts
+++ src/app/store/countries.selector.ts
@@ -10,10 +10,3 @@ export const getAllToSearch = createSelector(
   getCountriesState,
   (state) => state.countries
 );
-export const getCountriesLength = createSelector(
-  getCountriesState,
-  (state) => state.countriesLength
-);
-
-export const getSearchedCountries = (phrase: string) =>
-  createSelector(getCountriesState, (state) => {});
