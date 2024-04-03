3cc5cc925fc4b4e57c3ae2fc29043f74d65d1fed
Marek <marekkrupa03@gmail.com>
2024-04-03 19:24:20 +0200

    Abstract Constant class added

--- src/app/shared/constant.ts
+++ src/app/shared/constant.ts
@@ -1,1 +1,10 @@
-export const continents = ['Africa', 'Europa', 'Oceania', 'Asia', 'Americas'];
+export abstract class Constants {
+  static readonly paginatorConstants = {
+    defaultPageSize: 24,
+    pageSizeOptions: [8, 16, 24, 32],
+  };
+
+  static readonly filterConstants = {
+    regions: ['Africa', 'Europe', 'Oceania', 'Asia', 'Americas'],
+  };
+}
