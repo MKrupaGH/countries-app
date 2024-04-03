e5a019059cf6b9c849417fbb978cb312febcbb24
Marek <marekkrupa03@gmail.com>
2024-04-03 19:21:23 +0200

    Rout to my-countries added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomePageComponent } from './home/pages/home-page/home-page.component';
 import { CountriesPageComponent } from './countries/pages/countries-page/countries-page.component';
 import { LoginPageComponent } from './auth/login-page/login-page.component';
 import { SignupPageComponent } from './auth/signup-page/signup-page.component';
+import { MyCountriesComponent } from './countries/pages/my-countries/my-countries.component';
 
 export const routes: Routes = [
   {
@@ -18,6 +19,10 @@ export const routes: Routes = [
   {
     path: 'countries',
     component: CountriesPageComponent,
+  },
+  {
+    path: 'my-countries',
+    component: MyCountriesComponent,
   },
   {
     path: 'auth',
