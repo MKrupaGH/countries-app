22bf204df0b956c938d6d9e9ee488742ca30cc2f
Marek <marekkrupa03@gmail.com>
2024-04-05 16:54:50 +0200

    isLiked visualization added

--- src/app/shared/components/countries-list/country-detail/country-detail.component.ts
+++ src/app/shared/components/countries-list/country-detail/country-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Country } from '../../../models/country.model';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { CCA2 } from '../../../models/code.model';
 @Component({
   selector: 'app-country-detail',
   standalone: true,
@@ -13,4 +14,9 @@ import { CommonModule } from '@angular/common';
 })
 export class CountryDetailComponent {
   @Input() country!: Country;
+  @Output() onLike: EventEmitter<string> = new EventEmitter();
+
+  onClick(code: string) {
+    this.onLike.emit(code);
+  }
 }
