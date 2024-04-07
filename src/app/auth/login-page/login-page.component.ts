e2aa640a6f37525487937f01a33058dbed69c6fc
Marek <marekkrupa03@gmail.com>
2024-04-07 13:19:57 +0200

    login and signup components added

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { merge } from 'rxjs';
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
-
+  hide: boolean = true;
 }
--- src/app/auth/signup-page/signup-page.component.ts
+++ src/app/auth/signup-page/signup-page.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { merge } from 'rxjs';
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './signup-page.component.html',
-  styleUrl: './signup-page.component.scss'
+  styleUrl: './signup-page.component.scss',
 })
 export class SignupPageComponent {
-
+  hide: boolean = true;
 }
