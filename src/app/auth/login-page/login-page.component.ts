9443146423298bdaf9c62d1f4b22a93d47242b1d
Marek <marekkrupa03@gmail.com>
2024-04-08 14:52:23 +0200

    onEvent functions added

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -5,12 +5,19 @@ import {
   Validators,
   FormsModule,
   ReactiveFormsModule,
+  FormGroup,
 } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { merge } from 'rxjs';
+import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { setLoadingSpinner } from '../../store/shared/shared.actions';
+
+import * as Action from '../state/auth.actions';
 @Component({
   selector: 'app-login-page',
   standalone: true,
@@ -21,10 +28,26 @@ import { merge } from 'rxjs';
     MatIconModule,
     FormsModule,
     ReactiveFormsModule,
+    CommonModule,
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
   hide: boolean = true;
+
+  constructor(private authService: AuthService, private store: Store) {}
+  logForm = this.authService.log;
+
+  emailValidator = this.authService.showEmailErrors;
+  passwordValidator = this.authService.showPasswordErrors;
+  //login = this.authService.onLogin;
+
+  onLogin() {
+    //const { email, password } = this.logForm.value;
+    const email = this.logForm.value.email!;
+    const password = this.logForm.value.password!;
+    this.store.dispatch(setLoadingSpinner({ status: true }));
+    this.store.dispatch(Action.loginUser({ email, password }));
+  }
 }
--- src/app/auth/signup-page/signup-page.component.ts
+++ src/app/auth/signup-page/signup-page.component.ts
@@ -11,6 +11,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { merge } from 'rxjs';
+import { AuthService } from '../auth.service';
+import { Store } from '@ngrx/store';
+import { setLoadingSpinner } from '../../store/shared/shared.actions';
+
+import * as Action from '../state/auth.actions';
 @Component({
   selector: 'app-signup-page',
   standalone: true,
@@ -27,4 +32,19 @@ import { merge } from 'rxjs';
 })
 export class SignupPageComponent {
   hide: boolean = true;
+
+  constructor(private authService: AuthService, private store: Store) {}
+  logForm = this.authService.log;
+
+  emailValidator = this.authService.showEmailErrors;
+  passwordValidator = this.authService.showPasswordErrors;
+  //signup = this.authService.onSignup;
+
+  onSignup() {
+    //const { email, password } = this.logForm.value;
+    const email = this.logForm.value.email!;
+    const password = this.logForm.value.password!;
+    this.store.dispatch(setLoadingSpinner({ status: true }));
+    this.store.dispatch(Action.signUpUser({ email, password }));
+  }
 }
