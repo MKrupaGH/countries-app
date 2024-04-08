18d9fdc6ccb0133e2c3e81387d0fb29073be94d5
Marek <marekkrupa03@gmail.com>
2024-04-08 14:54:08 +0200

    AuthState added to interface

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -6,6 +6,9 @@ import { _sharedReducer } from './shared/shared.reducer';
 import { localStorageSync } from 'ngrx-store-localstorage';
 import { SHARED_STATE_NAME } from './shared/shared.selector';
 import { SharedState } from './shared/shared.state';
+import { AUTH_STATE_NAME } from '../auth/state/auth.selectors';
+import { AuthState } from '../auth/state/auth.state';
+import { _authReducer } from '../auth/state/auth.reducer';
 
 export function localStorageSyncReducer(rootReducer: any) {
   return localStorageSync({
@@ -18,8 +21,10 @@ export function localStorageSyncReducer(rootReducer: any) {
 export interface AppState {
   [COUNTRIES_STATE_NAME]: CountriesState;
   [SHARED_STATE_NAME]: SharedState;
+  [AUTH_STATE_NAME]: AuthState;
 }
 export const appReducer = {
   countries: localStorageSyncReducer(_countriesReducer),
   shared: _sharedReducer,
+  auth: _authReducer,
 };
